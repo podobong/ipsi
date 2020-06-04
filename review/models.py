8102db0e8e4fce181c06d29c2c7584626681e9d9
nuatmochoi <tjddn8770@naver.com>
2020-06-05 04:03:13 +0900

    Add: api document page by using drf-yasg

--- review/models.py
+++ review/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Review(models.Model):
     university = models.CharField(unique=True, max_length=20)
-    url = models.CharField(max_length=50)
+    url = models.URLField(max_length=50)
 
     def __str__(self):
         return str(self.university)
