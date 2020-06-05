757690ec74021ca15a529ebbec48e830ca355489
Kim Hyein <podobong00@gmail.com>
2020-06-05 07:56:19 +0000

    created suneung views

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('university.urls')),
-    path('', include('review.urls'))
+    path('', include('review.urls')),
+    path('', include('suneung.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
