98c3b7ba2932ad2cd283effb164e7a4cb42912c5
podobongbong <kim11560@snu.ac.kr>
2020-06-21 21:41:21 +0900

    created susi view

--- susi/views.py
+++ susi/views.py
@@ -1,13 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from susi.serializers import *
-
 
 class SusiScheduleList(APIView):
     serializer_class = NewSusiScheduleSerializer
 
     def get(self, request):
         schedules = SusiSchedule.objects.all()
-        serializer = NewSusiScheduleSerializer(schedules, many=True)
-        return Response(serializer.data)
+        schedule_list = []
+        for schedule in schedules:
+            schedule_info = {
+                'university': schedule.susi.university.name,
+                'sj': '수시',
+                'jh': schedule.susi.name,
+                'major_block': schedule.major_block.name,
+                'description': schedule.description,
+                'start_date': schedule.start_date,
+                'end_date': schedule.end_date,
+            }
+            # TODO: add 'favorite'
+            schedule_list.append(schedule_info)
+        return Response(schedule_list)
+
